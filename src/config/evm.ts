e17d2b3ef81e62afabc0645a17d0b66f30039e0f
waynewyang <yangwei.hit@gmail.com>
2024-01-05 13:45:07 +0800

    fix: 🐛 main evm using MAIN_LOTUS_PROVIDER_URL

--- src/config/evm.ts
+++ src/config/evm.ts
@@ -54,8 +54,8 @@ export const datasetMetadataEvm_Calibration = new DatasetMetadataEvm(
 
 export const datasetMetadataEvm_Main = new DatasetMetadataEvm(
     datasetMetaAbi,
-    "0x023e4b966b943c830b7d4cae84fe761641b29003",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    "0x7777777777777777777777777777777777777777",
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const datasetRequirementEvm_Calibration = new DatasetRequirementEvm(
@@ -67,7 +67,7 @@ export const datasetRequirementEvm_Calibration = new DatasetRequirementEvm(
 export const datasetRequirementEvm_Main = new DatasetRequirementEvm(
     datasetsRequirementAbi,
     "0x7777777777777777777777777777777777777700",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const datasetProofEvm_Calibration = new DatasetProofEvm(
@@ -79,7 +79,7 @@ export const datasetProofEvm_Calibration = new DatasetProofEvm(
 export const datasetProofEvm_Main = new DatasetProofEvm(
     datasetsProofAbi,
     "0x7777777777777777777777777777777777777701",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const datasetChallengeEvm_Calibration = new DatasetChallengeEvm(
@@ -91,7 +91,7 @@ export const datasetChallengeEvm_Calibration = new DatasetChallengeEvm(
 export const datasetChallengeEvm_Main = new DatasetChallengeEvm(
     datasetsChallengeAbi,
     "0x7777777777777777777777777777777777777702",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const matchingMetadataEvm_Calibration = new MatchingMetadataEvm(
@@ -103,7 +103,7 @@ export const matchingMetadataEvm_Calibration = new MatchingMetadataEvm(
 export const matchingMetadataEvm_Main = new MatchingMetadataEvm(
     matchingsAbi,
     "0x7777777777777777777777777777777777777703",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const matchingTargetEvm_Calibration = new MatchingTargetEvm(
@@ -115,7 +115,7 @@ export const matchingTargetEvm_Calibration = new MatchingTargetEvm(
 export const matchingTargetEvm_Main = new MatchingTargetEvm(
     matchingsTargetAbi,
     "0x7777777777777777777777777777777777777704",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const matchingBidsEvm_Calibration = new MatchingBidsEvm(
@@ -127,7 +127,7 @@ export const matchingBidsEvm_Calibration = new MatchingBidsEvm(
 export const matchingBidsEvm_Main = new MatchingBidsEvm(
     matchingsBidsAbi,
     "0x7777777777777777777777777777777777777705",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const storagesEvm_Calibration = new StoragesEvm(
@@ -139,7 +139,7 @@ export const storagesEvm_Calibration = new StoragesEvm(
 export const storagesEvm_Main = new StoragesEvm(
     storagesAbi,
     "0x7777777777777777777777777777777777777706",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const datacapsEvm_Calibration = new DatacapsEvm(
@@ -151,7 +151,7 @@ export const datacapsEvm_Calibration = new DatacapsEvm(
 export const datacapsEvm_Main = new DatacapsEvm(
     datacapsAbi,
     "0x7777777777777777777777777777777777777707",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const escrowEvm_Calibration = new EscrowEvm(
@@ -163,7 +163,7 @@ export const escrowEvm_Calibration = new EscrowEvm(
 export const escrowEvm_Main = new EscrowEvm(
     escrowAbi,
     "0x7777777777777777777777777777777777777708",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const filplusEvm_Calibraion = new FilplusEvm(
@@ -175,7 +175,7 @@ export const filplusEvm_Calibraion = new FilplusEvm(
 export const filplusEvm_Main = new FilplusEvm(
     filplusAbi,
     "0x7777777777777777777777777777777777777709",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
 
 export const rolesEvm_Calibration = new RolesEvm(
@@ -187,5 +187,5 @@ export const rolesEvm_Calibration = new RolesEvm(
 export const rolesEvm_Main = new RolesEvm(
     rolesAbi,
     "0x7777777777777777777777777777777777777710",
-    process.env.CALIBRATION_LOTUS_PROVIDER_URL
+    process.env.MAIN_LOTUS_PROVIDER_URL
 )
