9fe79ecdae2313fd101422fdbdcb41fb106c96c6
sirius <sirius.yim@gmail.com>
2024-03-07 17:18:26 +0800

    feat: 🎸 update dataswapjs version to 0.23.1
    ✅ Closes: #157

--- src/backgroundTask/storager/index.ts
+++ src/backgroundTask/storager/index.ts
@@ -498,7 +498,6 @@ export class Storager implements IStorager {
                                     matchingBids: this.context.evm.matchingBids,
                                     origionMatchingBid:
                                         selected.params as MatchingBid,
-                                    network: this.context.chain.network,
                                 }
                             )
                         )
